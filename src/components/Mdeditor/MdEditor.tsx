412e04a79b99767e20ffbc56275f874c3756a2bf
HeeSungLee <102288399+lheesung@users.noreply.github.com>
2023-04-12 20:35:01 +0900

    tool bar 옵션 설정

--- src/components/Mdeditor/MdEditor.tsx
+++ src/components/Mdeditor/MdEditor.tsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
-function App(): JSX.Element {
+export default function MdEditor(): JSX.Element {
   const eRef = useRef<Editor | null>(null);
   const handleSubmitBtnHTML = (): void => {
     console.log(eRef.current?.getInstance().getHTML()); // 입력창에 입력한 내용을 HTML 태그형식으로 반환
@@ -18,14 +18,20 @@ function App(): JSX.Element {
       <Editor
         ref={eRef}
         placeholder='내용을 입력해주세요.'
-        previewStyle="tab" // 미리보기 스타일
+        previewStyle="vertical" // 미리보기 스타일
         height='300px' // 수정 창 높이
         initialEditType="markdown" // 초기 입력모드 설정(default: markdown) / What You See Is What You Get
+        toolbarItems={[
+            // 툴바 옵션 설정
+            ['heading', 'bold', 'italic', 'strike'],
+            ['hr', 'quote'],
+            ['ul', 'ol', 'task', 'indent', 'outdent'],
+            ['table', 'image', 'link'],
+            ['code', 'codeblock']
+          ]}
       />
       <button onClick={handleSubmitBtnHTML}>get HTML</button>
       <button onClick={handleSubmitBtnMD}>get Markdown</button>
     </>
   );
 }
-
-export default App;
